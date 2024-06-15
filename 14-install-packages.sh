c8889c8e6ac29eb964aba86aad7df75ba1fa01f9
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-06-15 18:02:08 -0400

    Update 14-install-packages.sh

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -18,6 +18,30 @@
 
 # special variable to get all params --> $@
 
+# USERID=$(id -u)
+# if [ $USERID -ne 0 ]
+# then
+#     echo "Please run this script with root access."
+#     exit 1 # manually exit if error comes.
+# else
+#     echo "You are super user."
+# fi
+
+# echo "All packages: $@"
+
+# Output: 
+#     [ ec2-user@ip-172-31-27-208 ~/learn-shell-scripting ]$ sudo sh 14-install-packages.sh
+#     You are super user.
+#     All packages: 
+
+#     [ ec2-user@ip-172-31-27-208 ~/learn-shell-scripting ]$ sudo sh 14-install-packages.sh mysql gcc chrony
+#     You are super user.
+#     All packages: mysql gcc chrony
+
+#     [ ec2-user@ip-172-31-27-208 ~/learn-shell-scripting ]$
+
+
+# Now giving the packages in loops:
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
@@ -27,4 +51,7 @@ else
     echo "You are super user."
 fi
 
-echo "All packages: $@"
+for i in $@
+do 
+    echo "packages to install: $i"
+done
