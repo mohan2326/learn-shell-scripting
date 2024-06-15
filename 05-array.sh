8b4d91b6b26ff3253e0e6434e6ff764247331f40
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-06-15 15:30:41 -0400

    Create 05-array.sh

---
+++ 05-array.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#For example considering these are in array format
+TECHNOLOGIES=("AWS" "AZURE" "GCP") #list of values
+
+#List always starts with 0 
+#Size of above array is 3
+#Index are 0,1,2
+
+echo "First technology is: ${TECHNOLOGIES[0]}"
+echo "Secont technology is: ${TECHNOLOGIES[1]}"
+echo "Third technology is: ${TECHNOLOGIES[2]}"
+echo "All technology is: ${TECHNOLOGIES[@]}" #here @ is everything which will print all
+
+
+# Data types
+# ------------------
+# integer (number)
+# float (number)
+# decimal (number)
+# string (text)
+# boolean --> true/false ... 1=true, 0=false
+# array ()
+# arraylist
+# set, map
+
+# array --> list of values
+# if you define a variable as array, it can hold list of values...
