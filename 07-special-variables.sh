0095d860223ec5145d21d50f1807fdd85ebc3e1c
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-06-15 15:47:48 -0400

    Update 07-special-variables.sh

--- 07-special-variables.sh
+++ 07-special-variables.sh
@@ -58,6 +58,16 @@ echo "Hostname: $HOSTNAME"
 # Hostname: ip-172-31-27-208.ec2.internal
 
 echo "Process ID of the current shell script: $$"
+# output:
+# [ ec2-user@ip-172-31-27-208 ~/learn-shell-scripting ]$ sh 07-special-variables.sh devops aws
+# All variables: devops aws
+# Number of variables passed: 2
+# Script Name: 07-special-variables.sh
+# Current working directory: /home/ec2-user/learn-shell-scripting
+# Home directory of current user: /home/ec2-user
+# Which user is running this script: ec2-user
+# Hostname: ip-172-31-27-208.ec2.internal
+# Process ID of the current shell script: 1831 #this is the process ID created by Linux
 
-# sleep 60 &
-# echo "Process ID of last background command: $!"
+#sleep 60 & #If you want investigate which process ID on the linux
+echo "Process ID of last background command: $!"
