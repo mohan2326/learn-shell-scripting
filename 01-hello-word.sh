ecf597af4f769eb7b009476b56a072574e039861
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-06-15 14:49:13 -0400

    Update 01-hello-word.sh

--- 01-hello-word.sh
+++ 01-hello-word.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 echo "This is my 1st message using shell script"
+
+######## Notes ###############
+# Shell Script
+# ---------------
+# 1st line -->#!/bin/bash
+
+# shebang is the location interpreter, commands written inside shell script is interpreted and executed by this shebang
+
+# bash shell == shell
+
+# how to execute shell script
+# ---------------------
+# sh <script>
+# bash <script>
+# ./<script-name> --> this should have execute permission
+# X --> running the command/script
