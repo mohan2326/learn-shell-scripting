a43633ac88b13164cdb6450fbf62c9b28750d17e
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-06-15 17:49:42 -0400

    Create 13-loops.sh

---
+++ 13-loops.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# loops
+# -----------
+# for(int i=0; i<20; i++){
+# 	print $i;
+# }
+
+#https://stackoverflow.com/questions/1445452/shell-script-for-loop-syntax
+
+for i in {1..20} # it will loop from 1 to 20
+do
+    echo $i
+done
