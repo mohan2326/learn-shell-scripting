7fe49596023a21896802a78699a46f49e7ed7567
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-06-15 15:42:12 -0400

    Update 07-special-variables.sh

--- 07-special-variables.sh
+++ 07-special-variables.sh
@@ -15,9 +15,14 @@ echo "Number of variables passed: $#"
 
 
 echo "Script Name: $0"
+#output
+# [ ec2-user@ip-172-31-27-208 ~/learn-shell-scripting ]$ sh 07-special-variables.sh devops aws
+# All variables: devops aws
+# Number of variables passed: 2
+# Script Name: 07-special-variables.sh
 
+echo "Current working directory: $PWD"
 
-# echo "Current working directory: $PWD"
 # echo "Home directory of current user: $HOME"
 # echo "Which user is running this script: $USER"
 # echo "Hostname: $HOSTNAME"
