ebc6aab9c0105a16bd96422c405a05d49fb6c89a
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-06-15 16:49:14 -0400

    Update 10-functions.sh

--- 10-functions.sh
+++ 10-functions.sh
@@ -23,11 +23,55 @@
 
 # Modifying the same code used the installtions with funtions:
 
+# USERID=$(id -u)
+
+# VALIDATE(){
+#     echo "Exit status: $1"
+#     echo "What are you doing: $2"
+# }
+
+# if [ $USERID -ne 0 ]
+# then
+#     echo "Please run this script with root access."
+#     exit 1 # manually exit if error comes.
+# else
+#     echo "You are super user."
+# fi
+
+# dnf install mysql -y
+# VALIDATE $? "Installing MySQL"
+
+# dnf install git -y
+# VALIDATE $? "Installing GIT"
+
+# Output:
+#     [ ec2-user@ip-172-31-27-208 ~/learn-shell-scripting ]$ sudo sh 10-functions.sh
+#     You are super user.
+#     Last metadata expiration check: 0:21:03 ago on Sat Jun 15 20:24:05 2024.
+#     Package mysql-8.0.36-1.el9_3.x86_64 is already installed.
+#     Dependencies resolved.
+#     Nothing to do.
+#     Complete!
+#     Exit status: 0
+#     What are you doing: Installing MySQL
+#     Last metadata expiration check: 0:21:04 ago on Sat Jun 15 20:24:05 2024.
+#     Package git-2.43.0-1.el9.x86_64 is already installed.
+#     Dependencies resolved.
+#     Nothing to do.
+#     Complete!
+#     Exit status: 0
+#     What are you doing: Installing GIT
+
 USERID=$(id -u)
 
 VALIDATE(){
-    echo "Exit status: $1"
-    echo "What are you doing: $2"
+   if [ $1 -ne 0 ]
+   then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
 }
 
 if [ $USERID -ne 0 ]
