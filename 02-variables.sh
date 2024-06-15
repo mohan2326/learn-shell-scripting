91bdaa955190efc7d8db8f82b301697e2ea5e37d
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-06-15 14:53:55 -0400

    commit

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Lets assume converstion between to people 
+
+echo "Linux:: Hello SRE, How are you?"
+echo "SRE:: Hi Linux, I am fine. How are you?"
+echo "Linux: I am fine too. how is your work?"
+echo "SRE:: not bad. I am thinking to upgrade to DevOps"
