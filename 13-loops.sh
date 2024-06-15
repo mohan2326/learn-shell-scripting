6abbb8742aaa546667f26a51928b9cf4b2edc016
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-06-15 17:50:39 -0400

    Update 13-loops.sh

--- 13-loops.sh
+++ 13-loops.sh
@@ -12,3 +12,26 @@ for i in {1..20} # it will loop from 1 to 20
 do
     echo $i
 done
+
+# Output:
+#     [ ec2-user@ip-172-31-27-208 ~/learn-shell-scripting ]$ sh 13-loops.sh
+#     1
+#     2
+#     3
+#     4
+#     5
+#     6
+#     7
+#     8
+#     9
+#     10
+#     11
+#     12
+#     13
+#     14
+#     15
+#     16
+#     17
+#     18
+#     19
+#     20
