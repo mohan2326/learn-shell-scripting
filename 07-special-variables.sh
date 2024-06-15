b4091707a29dfb3bee25b2549af1adfca3661a1d
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-06-15 15:52:22 -0400

    Update 07-special-variables.sh

--- 07-special-variables.sh
+++ 07-special-variables.sh
@@ -70,4 +70,21 @@ echo "Process ID of the current shell script: $$"
 # Process ID of the current shell script: 1831 #this is the process ID created by Linux
 
 sleep 60 & #If you want investigate which process ID on the linux
+# "&" is used to send the command to backgroud to check status
 echo "Process ID of last background command: $!"
+
+# output:
+# [ ec2-user@ip-172-31-27-208 ~/learn-shell-scripting ]$ sh 07-special-variables.sh devops aws
+# All variables: devops aws
+# Number of variables passed: 2
+# Script Name: 07-special-variables.sh
+# Current working directory: /home/ec2-user/learn-shell-scripting
+# Home directory of current user: /home/ec2-user
+# Which user is running this script: ec2-user
+# Hostname: ip-172-31-27-208.ec2.internal
+# Process ID of the current shell script: 1930
+# Process ID of last background command: 1931
+# #Backgroud command
+# [ ec2-user@ip-172-31-27-208 ~ ]$ ps -ef |grep sleep
+# ec2-user    1931       1  0 19:49 pts/0    00:00:00 sleep 60
+# ec2-user    1939    1880  0 19:49 pts/1    00:00:00 grep --color=auto sleep
