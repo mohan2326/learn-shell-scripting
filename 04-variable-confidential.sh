3b0923a748fbd3db5ec0ea878862208228160ebd
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-06-15 15:15:49 -0400

    Create 04-variable-confidential.sh

---
+++ 04-variable-confidential.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Please enter username::"
+
+read USERNAME #here USERNAME IS variable
+
+echo "Please enter password::"
+
+read PASSWORD
+
+echo "Username is: $USERNAME, Password is: $PASSWORD"
+
+# confidential information
+# -----------------------
+# We want user to enter manually before execution
