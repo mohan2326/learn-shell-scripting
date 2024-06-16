de9be962e653526a6fe040210108aa1f31dea13d
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-06-16 00:52:27 -0400

    Update 15-currest-script.sh

--- 15-currest-script.sh
+++ 15-currest-script.sh
@@ -29,7 +29,7 @@ COURSE="DevOps from Current Script-1"
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
-#./16-other-script.sh
+./16-other-script.sh
 
 echo "After calling other script, course: $COURSE"
 #echo "Process ID of current shell script: $$"
