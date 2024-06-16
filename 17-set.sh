9c11ae6014a97e965f056596223b63eaf9903bd6
Mohan <165742075+mohan2326@users.noreply.github.com>
2024-06-16 01:47:28 -0400

    Update 17-set.sh

--- 17-set.sh
+++ 17-set.sh
@@ -10,11 +10,11 @@
 
 # set -e
 
-# failure(){
-#     echo "Failed at $1: $2"
-# }
+failure(){
+    echo "Failed at $1: $2"
+}
 
-# trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
 
 USERID=$(id -u) #ERR
 
@@ -30,3 +30,16 @@ dnf install mysfaffql -y
 dnf install git -y
 
 echo "is script proceeding?"
+
+# Output:
+#     [ ec2-user@ip-172-31-27-208 ~/learn-shell-scripting ]$ sudo sh 17-set.sh 
+#     You are super user.
+#     Last metadata expiration check: 2:15:01 ago on Sun Jun 16 03:31:29 2024.
+#     No match for argument: mysfaffql
+#     Error: Unable to find a match: mysfaffql
+#     Last metadata expiration check: 2:15:03 ago on Sun Jun 16 03:31:29 2024.
+#     Package git-2.43.0-1.el9.x86_64 is already installed.
+#     Dependencies resolved.
+#     Nothing to do.
+#     Complete!
+#     is script proceeding?
